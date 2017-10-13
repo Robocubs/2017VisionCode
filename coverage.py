fb5e28ef6d450476d99d8720b862cccc461a8f7a
Nicholas Hubbard <hubbardn95@gmail.com>
2017-10-12 21:13:54 -0400

    Add Stats to JSON file

--- coverage.py
+++ coverage.py
@@ -26,9 +26,16 @@ for x, y in returns.items():
 	else:
 		report[x] = True
 		passes = passes + 1
+# Add stats to JSON.
+report["stats"] = {
+	"passes": passes,
+	"fails": total - passes,
+	"total": total,
+	"percentage": percentage(passes, total)
+}
 # Below we generate a coverage report as `cvcov.json`.
 with open("cvcov.json", "w") as file:
-	file.write(json.dumps(report, sort_keys=True))
+	file.write(json.dumps(report, sort_keys=True, indent=4))
 with open("badge.svg", "w") as file:
 	file.write("<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" width=\"94\" height=\"20\"><linearGradient id=\"b\" x2=\"0\" y2=\"100%\"><stop offset=\"0\" stop-color=\"#bbb\" stop-opacity=\".1\"/><stop offset=\"1\" stop-opacity=\".1\"/></linearGradient><clipPath id=\"a\"><rect width=\"94\" height=\"20\" rx=\"3\" fill=\"#fff\"/></clipPath><g clip-path=\"url(#a)\"><path fill=\"#555\" d=\"M0 0h59v20H0z\"/><path fill=\"#4c1\" d=\"M59 0h35v20H59z\"/><path fill=\"url(#b)\" d=\"M0 0h94v20H0z\"/></g><g fill=\"#fff\" text-anchor=\"middle\" font-family=\"DejaVu Sans,Verdana,Geneva,sans-serif\" font-size=\"11\"><text x=\"29.5\" y=\"15\" fill=\"#010101\" fill-opacity=\".3\">cvcov</text><text x=\"29.5\" y=\"14\">cvcov</text><text x=\"75.5\" y=\"15\" fill=\"#010101\" fill-opacity=\".3\">" + str(percentage(passes, total)) + "%</text><text x=\"75.5\" y=\"14\">" + str(percentage(passes, total)) + "%</text></g></svg>")
 print("\n" + Fore.GREEN + "Passes\t" + str(passes))
